0b90a38e6f12b1c8310377e3e967baa3ff50c37d
Bay Batsaikhan <bay.infosec@gmail.com>
2026-06-06 00:29:45 -0700

    Create constants.js

---
+++ src/lib/constants.js
@@ -0,0 +1,13 @@
+export const NAVY = "#162038";
+export const ORANGE = "#E8621A";
+export const GOLD = "#EFAB25";
+export const BORDER = "#E2DDD6";
+export const TSEC = "#8A8498";
+export const BG = "#F7F4EF";
+export const SANS = "'DM Sans', sans-serif";
+export const SERIF = "'Cormorant Garamond', serif";
+
+export const EMAILJS_SERVICE = import.meta.env.VITE_EMAILJS_SERVICE_ID;
+export const EMAILJS_INVITE_TPL = import.meta.env.VITE_EMAILJS_INVITE_TEMPLATE_ID;
+export const EMAILJS_ANN_TPL = import.meta.env.VITE_EMAILJS_ANNOUNCEMENT_TEMPLATE_ID;
+export const EMAILJS_PUBLIC = import.meta.env.VITE_EMAILJS_PUBLIC_KEY;
