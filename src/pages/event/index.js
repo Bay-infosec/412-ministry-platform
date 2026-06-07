085d84572fe3ad4e2b22be4f3cd9e84e3ca3338f
Bay Batsaikhan <bay.infosec@gmail.com>
2026-06-06 17:30:26 -0700

    Update index.js

--- src/pages/event/index.js
+++ src/pages/event/index.js
@@ -1,1 +1,5 @@
 export { default as EventHome } from "./EventHome.jsx";
+export { default as MyTeam } from "./MyTeam.jsx";
+export { default as PrayerChain } from "./PrayerChain.jsx";
+export { default as TheFour } from "./TheFour.jsx";
+export { default as FieldGuide } from "./FieldGuide.jsx";
